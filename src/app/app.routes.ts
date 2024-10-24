3ba5c1116242b8478f159e79fdc738503acdde02
abrumay <abrumay@hotmail.com>
2024-10-24 11:11:29 -0300

    Cambios Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { BasicPageComponent } from './basic-page/basic-page.component';
 import { LoginComponent } from './login/login.component';
 import { PremiumPageComponent } from './premium-page/premium-page.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'admin', component: AdminPageComponent },
   { path: 'basic-client', component: BasicPageComponent },
@@ -14,8 +13,4 @@ const routes: Routes = [
   { path: '**', redirectTo: '' } // Redirecciona a la página de login si la ruta no se encuentra
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
 export class AppRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+// Combina provideRouter en appConfig
+const config = {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    provideRouter(routes) // Añade provideRouter aquí
+  ]
+};
+
+bootstrapApplication(AppComponent, config)
   .catch((err) => console.error(err));
